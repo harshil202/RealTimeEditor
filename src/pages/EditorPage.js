9543deb622ea50dff0b6beccd291ebdf9b0d4695
harshilhirpara <hirparah499@gmail.com>
2022-11-13 11:17:29 +0530

    add file

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -95,13 +95,7 @@ const EditorPage = () => {
         <div className="mainWrap">
             <div className="aside">
                 <div className="asideInner">
-                    <div className="logo">
-                        <img
-                            className="logoImage"
-                            src="/code-sync.png"
-                            alt="logo"
-                        />
-                    </div>
+                    
                     <h3>Connected</h3>
                     <div className="clientsList">
                         {clients.map((client) => (
